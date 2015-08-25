67978e2c18b9ceb666ece1650380f4a65ab44cc8
Nick <thuydx55vnu@gmail.com>
2015-08-25 21:47:30 +0700

    api userInfo

--- routes/api.js
+++ routes/api.js
@@ -8,29 +8,6 @@ var User        = require('../lib/models/User');
 
 var UserController = require('../lib/controllers/UserController');
 
-function isAuthenticated(req, res, next) {
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    // decode token
-    if (token) {
-        // verifies secret and checks exp
-        jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {      
-            if (err) {
-                return res.json(Utils.buildRes(false, 'Failed to authenticate token.', null));
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;    
-                next();
-            }
-        });
-    } else {
-        // if there is no token
-        // return an error
-        return res.status(403).send(Utils.buildRes(false, 'No token provided.', null));
-    }
-};
-
 // router.get('/users', isAuthenticated);
 router.get('/users', function(req, res) {
     User.find({}, function(err, users) {
@@ -40,6 +17,7 @@ router.get('/users', function(req, res) {
 
 router.post('/login', UserController.login);
 router.post('/register', UserController.register);
-router.post('/logout', isAuthenticated, UserController.logout);
+router.post('/logout', Utils.verify, UserController.logout);
+router.get('/me', Utils.verify, UserController.userInfo);
 
 module.exports = router;
