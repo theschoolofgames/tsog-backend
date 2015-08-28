44f90c8ccef87b0a26d7a1b68b658dd896dc5a76
Nick <thuydx55vnu@gmail.com>
2015-08-28 14:23:51 +0700

    api get_schools

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,8 @@ var passwordHash    = require('password-hash');
 var Utils       = require('../lib/Utils');
 var User        = require('../lib/models/User');
 
-var UserController = require('../lib/controllers/UserController');
+var UserController      = require('../lib/controllers/UserController');
+var SchoolController    = require('../lib/controllers/SchoolController');
 
 // router.get('/users', isAuthenticated);
 router.get('/users', function(req, res) {
@@ -19,5 +20,7 @@ router.post('/login', UserController.login);
 router.post('/register', UserController.register);
 router.post('/logout', Utils.verify, UserController.logout);
 router.get('/me', Utils.verify, UserController.userInfo);
+
+router.get('/get_schools', SchoolController.getSchools);
 
 module.exports = router;
