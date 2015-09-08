e0c97f5032afb4a6ccedbeaa86d9d981664c4be4
Nick <thuydx55vnu@gmail.com>
2015-09-08 23:54:53 +0700

    getAccounts API

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,8 @@ var passwordHash    = require('password-hash');
 var Utils       = require('../lib/Utils');
 var Token       = require('../lib/Token');
 var User        = require('../lib/models/User');
+var School      = require('../lib/models/School');
+var UserAvatar  = require('../lib/models/UserAvatar');
 
 var UserController          = require('../lib/controllers/UserController');
 var SchoolController        = require('../lib/controllers/SchoolController');
@@ -19,14 +21,44 @@ router.get('/users', function(req, res) {
     });
 });
 
-router.post('/login', UserController.login);
-router.post('/register', UserController.register);
-router.post('/logout', Token.verify, UserController.logout);
-router.post('/change_password', Token.verify, UserController.changePassword);
+router.get('/seed', function(req, res) {
+    School.find({}, function(err, schools) {
+        for (var i = 0; i < 100; i++) {
+            var newAvatar = new UserAvatar({
+                sex: Math.floor(Math.random()*2),
+                skinId: Math.floor(Math.random()*5),
+                hairId: Math.floor(Math.random()*5),
+                eyeId: Math.floor(Math.random()*5),
+                mouthId: Math.floor(Math.random()*5),
+                noseId: Math.floor(Math.random()*5)
+            }); 
+
+            newAvatar.save(function(err) {
+                var newUser = new User({ 
+                    password: 1,
+                    school: schools[Math.floor(Math.random() * schools.length)],
+                    avatar: newAvatar
+                });
+
+                newUser.save(function(err) {
+
+                });
+            });
+        }
+
+        res.json("OK");
+    });
+});
+
+// router.post('/login', UserController.login);
+// router.post('/register', UserController.register);
+// router.post('/logout', Token.verify, UserController.logout);
+// router.post('/change_password', Token.verify, UserController.changePassword);
 router.get('/me', Token.verify, UserController.userInfo);
 
 router.get('/schools', SchoolController.getSchools);
-router.post('/select_school', Token.verify, SchoolController.selectSchool);
+router.get('/accounts', SchoolController.getAccounts);
+// router.post('/select_school', Token.verify, SchoolController.selectSchool);
 
 router.get('/games', GameController.getGames);
 
